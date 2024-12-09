ee185261832e830b8f0c233644ceed1cbf94b1fd
sbin0819 <sbinha123@gmail.com>
2024-12-09 11:46:19 +0900

    feat: api 작업

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './search/search.module';
+import { CategoriesModule } from './categories/categories.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SearchModule],
+  imports: [SearchModule, CategoriesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
